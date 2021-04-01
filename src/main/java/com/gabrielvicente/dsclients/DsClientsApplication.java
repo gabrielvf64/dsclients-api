e58d2caae4f6b84d09737dc64b0c836ad5316b23
gabrielvf64 <gabrielvf64@gmail.com>
2021-03-31 21:04:15 -0300

    feat: initial commit

---
+++ src/main/java/com/gabrielvicente/dsclients/DsClientsApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielvicente.dsclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientsApplication.class, args);
+	}
+
+}
